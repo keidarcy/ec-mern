7bcd2d90a084eeb3c754e1302cb89bd96ee49815
XING YAHAO <keidarcy.1015@gmail.com>
2020-10-10 22:20:34 +0900

    update

--- frontend/src/actions/productActions.ts
+++ frontend/src/actions/productActions.ts
@@ -1,10 +1,14 @@
 import { PRODUCT_ACTIONS } from '../types';
 import axios from 'axios';
 import { Dispatch } from 'redux';
-import { ProductActionTypes } from '../reducers/productReducers';
+import {
+  ProductActionTypes,
+  ProductDetailsActionTypes,
+  ReviewType
+} from '../reducers/productReducers';
 import { ProductType } from '../components/Product';
 
-export const listProducts = async (dispatch: Dispatch<ProductActionTypes>) => {
+export const listProducts = () => async (dispatch: Dispatch<ProductActionTypes>) => {
   try {
     dispatch({ type: PRODUCT_ACTIONS.PRODUCT_LIST_REQUEST });
     const { data } = await axios.get<ProductType[]>('/api/products');
@@ -16,6 +20,28 @@ export const listProducts = async (dispatch: Dispatch<ProductActionTypes>) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_ACTIONS.PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+    });
+  }
+};
+
+export const listProductDetails = (id: string) => async (
+  dispatch: Dispatch<ProductDetailsActionTypes>
+) => {
+  try {
+    dispatch({ type: PRODUCT_ACTIONS.PRODUCT_DETAILS_REQUEST });
+    const { data } = await axios.get<ProductType>(`/api/products/${id}`);
+
+    dispatch({
+      type: PRODUCT_ACTIONS.PRODUCT_DETAILS_SUCCESS,
+      payload: data
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_ACTIONS.PRODUCT_DETAILS_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
--- frontend/src/reducers/productReducers.ts
+++ frontend/src/reducers/productReducers.ts
@@ -6,6 +6,14 @@ export type ProductActionTypes =
   | { type: PRODUCT_ACTIONS.PRODUCT_LIST_SUCCESS; payload: ProductType[] }
   | { type: PRODUCT_ACTIONS.PRODUCT_LIST_FAIL; payload: Error };
 
+export type ProductDetailsActionTypes =
+  | { type: PRODUCT_ACTIONS.PRODUCT_DETAILS_REQUEST }
+  | {
+      type: PRODUCT_ACTIONS.PRODUCT_DETAILS_SUCCESS;
+      payload: ProductType;
+    }
+  | { type: PRODUCT_ACTIONS.PRODUCT_DETAILS_FAIL; payload: Error };
+
 export interface ProductListStateType {
   loading: boolean;
   products: ProductType[];
@@ -27,6 +35,36 @@ export const productListReducer = (
     case PRODUCT_ACTIONS.PRODUCT_LIST_SUCCESS:
       return { loading: false, products: action.payload };
     case PRODUCT_ACTIONS.PRODUCT_LIST_FAIL:
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export interface ReviewType {
+  comment: string;
+}
+
+export const productDetailsInitialState = {
+  loading: false
+};
+
+export interface ProductDetailsStateType {
+  loading: boolean;
+  product?: ProductType;
+  error?: Error;
+}
+
+export const productDetailsReducer = (
+  state: ProductDetailsStateType = productDetailsInitialState,
+  action: ProductDetailsActionTypes
+): ProductDetailsStateType => {
+  switch (action.type) {
+    case PRODUCT_ACTIONS.PRODUCT_DETAILS_REQUEST:
+      return { ...state, loading: true };
+    case PRODUCT_ACTIONS.PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: action.payload };
+    case PRODUCT_ACTIONS.PRODUCT_DETAILS_FAIL:
       return { ...state, loading: false, error: action.payload };
     default:
       return state;
