6b7954f8b2302ec07b88e21acd4d70533b689085
XING YAHAO <keidarcy.1015@gmail.com>
2020-11-10 13:00:31 +0900

    update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -34,6 +34,10 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(path.resolve(), 'frontend', 'build', 'index.html'))
   );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
 }
 
 app.use('/upload', express.static(path.join(path.resolve(), '/uploads')));
